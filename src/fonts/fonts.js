31919e25a9e343ba41df9e65611388c676a4c39a
himanshuvashist <35025815+himanshuvashist@users.noreply.github.com>
2024-03-16 10:11:30 +0530

    edited

---
+++ src/fonts/fonts.js
@@ -0,0 +1,7 @@
+import { Inter, The_Girl_Next_Door } from "next/font/google";
+
+export const interFont = Inter({ subsets: ["latin"] });
+export const theGirlNextDoorFont = The_Girl_Next_Door({
+  weight: ["400"],
+  subsets: ["latin"],
+});
